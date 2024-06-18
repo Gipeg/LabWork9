6322a66b5011d82311331d9a02c2f74fccc093f6
fisp <arenatkanovna@gmail.com>
2024-06-18 23:43:24 +0300

    Добавьте файлы проекта.

---
+++ Task1/Class1.cs
@@ -0,0 +1,87 @@
+﻿namespace Task1
+{
+    public class Order  //имена компонентов класса
+    {
+        public string _Name { get; set; }
+        public string _Address { get; set; }
+        public int _Summ { get; set; }
+
+        public Order() //первоначальный класс
+           : this("Неизвестна", "Неизвестен", 0) { }
+
+        public Order(string Name, string Address, int Summ) //присваивание переменным служебные имена
+        {
+            _Name = Name;
+            _Address = Address;
+            _Summ = Summ;
+        }
+
+        public static Order operator ++(Order order) => new Order //второй элемент класса не 0 а 1
+        {
+            _Name = order._Name,
+            _Address = order._Address,
+            _Summ = order._Summ + 1
+        };
+
+        public static Order operator +(Order a, Order b) => new Order //присваивание из Program.CS имён в класс
+        {
+            _Name = a._Name,
+            _Address = a._Address,
+            _Summ = a._Summ + b._Summ
+        };
+
+
+
+        //рассчёты операторов
+        public static bool operator &(Order a, Order b) => a._Summ == b._Summ;
+
+        public static bool operator ^(Order a, Order b) => a._Summ != b._Summ;
+
+        public static bool operator true(Order order) => order._Summ >= 0;
+
+        public static bool operator false(Order order) => order._Summ < 0;
+
+        public void Print() //вывод класса
+        {
+            Console.WriteLine($"Заказ: {_Name}, адрес: {_Address}, Сумма: {_Summ}");
+        }
+
+        interface IPrinter
+        {
+            void Print();
+        }
+
+        public class Kvadrat : IPrinter
+        {
+            public double Side;
+            public Kvadrat(double side)
+            {
+                Side = side;
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Квадрат со сторонами: {Side}");
+            }
+
+            class Order : IPrinter
+            {
+                public string Name;
+                public string Address;
+                public int Summ;
+
+                public Order(string name, string address, int summ)
+                {
+                    Name = name;
+                    Address = address;
+                    Summ = summ;
+                }
+
+                public void Print()
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using Task1;
+using static Task1.Order;
+{
+Order order = new();
+order.Print();
+Console.WriteLine("_____________");
+
+Console.WriteLine("Operators:"); //вывод операторов
+
+Order order1 = new() { _Summ = 0 };
+Console.WriteLine(order1._Summ);
+order1++;
+order1.Print();
+
+Console.WriteLine("________________________________"); //вывод заказа
+Order order2 = new() { _Summ = 2000, _Address = "ул. Пушкина д. Колотушкино", _Name = "Илья" };
+Order order3 = new() { _Summ = 500 };
+Console.WriteLine(order2._Summ);
+Console.WriteLine(order3._Summ);
+
+    Console.WriteLine($"Сторона: {Kvadrat.Side}");
+}
